8ee17dd3fab63ca63112f27ce3dc55a7c1b0003e
priyansh <priyansh-Mackbook@gmail.com>
2026-06-22 12:46:06 +0530

    added mobile store

--- src/services/mobileService.jsx
+++ src/services/mobileService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3001/mobiles';
+const API_BASE_URL = 'http://192.168.0.24:3001/mobiles';
 
 const mobileService = {
   // Get all mobiles
